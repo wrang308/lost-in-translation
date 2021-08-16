52ce1886fd1f273a57024805b77877357fa0f8a4
wrang308 <markus.wrang_94@hotmail.com>
2021-08-16 19:59:47 +0200

    added a new look for the startpage

--- src/components/StartUp/StartUpPage.jsx
+++ src/components/StartUp/StartUpPage.jsx
@@ -3,11 +3,16 @@ const StartUpPage = () => {
 
 
   return (
-    <div className="Login">
-
-        <input type="text" placeholder="username"/>
-        <button>Login</button>
-    </div>
+    <div className="container" onclick="onclick">
+  <div class="top"></div>
+  <div class="bottom"></div>
+  <div class="center">
+    <h2>Please sign in</h2>
+    <input type="text" placeholder="username"/>
+    <button class="loginButton">Login</button>
+    <h2>&nbsp;</h2>
+  </div>
+</div>
   )
 }
 

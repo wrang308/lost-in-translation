cb3f29e546e6b843c0bc93c050a559c9a5b7d9be
wrang308 <markus.wrang_94@hotmail.com>
2021-08-17 17:25:27 +0200

    fixing some merge stuff

--- src/components/Translation/TranslationPage.jsx
+++ src/components/Translation/TranslationPage.jsx
@@ -8,10 +8,13 @@ const TranslationPage = () => {
   const [clicked, setClicked] = useState(false)
 
   const handleTranslateBtn = () => {
+    if(translationText.length > 40){
+      alert("max 40 characters long text");
+      return;
+    }
     
     console.log(translationText)
     setClicked(true)
-    //även automatiskt spara translations till db
   }
 
   if(!localStorage.getItem('username')){
@@ -23,40 +26,24 @@ const TranslationPage = () => {
       history.goBack(history.length-1)
   }
 
-<<<<<<< HEAD
   const handleTranslationTextChange = e => {
     setTranslationText(e.target.value);
   }
 
   return (
     <div className={styles.container}>
-    <div className={styles.top}></div>
-    <div className={styles.bottom}></div>
-    <div className={styles.center}>
-      <div className={styles.card}>
-        <input className={styles.translatorInput} type="text" name="translation" id="translation" placeholder="Please type what you wish to translate"  onChange={handleTranslationTextChange}/>
-
-        <button className={styles.translateBtn} onClick={handleTranslateBtn}>Translate</button>
-        {clicked === true && (
-          <div className={styles.translationContainer}></div>
-        )}
-        <button className={styles.translateBtn} onClick={handleLogOut}>log out</button>
-=======
-  return (
-    <div className={styles.container}>
       <div className={styles.top}></div>
       <div className={styles.bottom}></div>
       <div className={styles.center}>
         <div className={styles.card}>
-          <input className={styles.translatorInput} type="text" name="translation" id="translation" placeholder="Please type what you wish to translate" />
+          <input className={styles.translatorInput} type="text" name="translation" id="translation" placeholder="Please type what you wish to translate" onChange={handleTranslationTextChange}/>
 
           <button className={styles.translateBtn} onClick={handleTranslateBtn}>Translate</button>
           {clicked === true && (
             <div className={styles.translationContainer}></div>
           )}
-          <button className={styles.logOutBtn} onClick={handleLogOut}>log out</button>
+          <button className={styles.translateBtn} onClick={handleLogOut}>log out</button>
         </div>
->>>>>>> d8ffc481d9b4da5ac4f176a2945c6ccec4d27fb4
       </div>
     </div>
   )

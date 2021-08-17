d7fedf746ef9d139ef5797cfc00432c213b15b2d
wrang308 <markus.wrang_94@hotmail.com>
2021-08-17 11:58:46 +0200

    added functionality that saves the username in the localStorage

--- src/components/StartUp/StartUpPage.jsx
+++ src/components/StartUp/StartUpPage.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import { useHistory  } from 'react-router'
 import styles from '../StartUp/StartUpPage.module.css'
 
 const StartUpPage = () => {
   const history = useHistory();
+  const [username, setUsername] = useState('')
+
+  if(localStorage.getItem('username')){
+    history.push('/translator')
+  }
 
   const handleOnClick = () => {
-    history.push('/translator')
+    
+    if(username != ''){
+      localStorage.clear();
+      localStorage.setItem('username', username);
+      history.push('/translator')
+    }
+  }
+  const handleUsernameChange = e => {
+    setUsername(e.target.value);
   }
 
   return (
@@ -14,7 +28,7 @@ const StartUpPage = () => {
     <div className={styles.bottom}></div>
     <div className={styles.center}>
       <h2>Please sign in</h2>
-      <input type="text" placeholder="username"/>
+      <input type="text" placeholder="username" onChange={handleUsernameChange}/>
       <button className={styles.loginButton} onClick={handleOnClick}>Login</button>
     </div>
   </div>
--- src/components/Translation/TranslationPage.jsx
+++ src/components/Translation/TranslationPage.jsx
@@ -1,6 +1,19 @@
+import { useHistory  } from 'react-router'
 import styles from '../Translation/TranslationPage.module.css'
 
 const TranslationPage = () => {
+  const history = useHistory();
+
+  if(!localStorage.getItem('username')){
+    history.goBack(history.length-1)
+  }
+
+  const handleLogOut = () => {
+      localStorage.clear();
+      history.goBack(history.length-1)
+    
+  }
+
   return (
     <div className={styles.container}>
     <div className={styles.top}></div>
@@ -9,6 +22,7 @@ const TranslationPage = () => {
       <div className={styles.card}>
         <input className={styles.translatorInput} type="text" name="translation" id="translation" placeholder="Please type what you wish to translate" />
         <button className={styles.translateBtn}>Translate</button>
+        <button className={styles.translateBtn} onClick={handleLogOut}>log out</button>
       </div>
     </div>
     </div>

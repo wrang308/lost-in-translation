7c5d2b206d0b68bd4a6cf9bcccb383f39bf581f1
Katalina Palma <kata_92@hotmail.com>
2021-08-17 09:49:31 +0200

    added onclick event to redirect user to translator page

--- src/components/StartUp/StartUpPage.jsx
+++ src/components/StartUp/StartUpPage.jsx
@@ -1,6 +1,13 @@
+import { useHistory  } from 'react-router'
 import styles from '../StartUp/StartUpPage.module.css'
 
 const StartUpPage = () => {
+  const history = useHistory();
+
+  const handleOnClick = () => {
+    history.push('/translator')
+  }
+
   return (
   <div className={styles.container}>
     <div className={styles.top}></div>
@@ -8,7 +15,7 @@ const StartUpPage = () => {
     <div className={styles.center}>
       <h2>Please sign in</h2>
       <input type="text" placeholder="username"/>
-      <button className={styles.loginButton}>Login</button>
+      <button className={styles.loginButton} onClick={handleOnClick}>Login</button>
     </div>
   </div>
   )
